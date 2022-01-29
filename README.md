d9706b43a86d96aacc486c38ed5bc7c111f3709e
PADO <62028267+47PADO47@users.noreply.github.com>
2022-01-29 16:28:53 +0100

    📝✨🔥📄🙈💡🔖 - v1.1.0
    📝 - Edited README.md
    ✨ - Can fetch with http and https
    🔥 - "CheckDir()" now private
    📄 - Added MIT license
    🙈 - Added .gitignore
    💡 - Better JSDoc
    🔖 - v1.1.0

--- README.md
+++ README.md
@@ -1,5 +1,34 @@
-# Installation
+# Downloader
+Just a simple NPM package to download things off the internet
+
+## Table Of Contents
+  - [Installation](#installation)
+  - [Example](#example)
+
+## Installation
 
 ```sh
-npm install node-downloader.js
+    npm install node-downloader.js
+```
+
+## Example
+
+```javascript
+    const { Downloader } = require('node-downloader.js');
+
+    (() => {
+        const options = {
+            path: './downloads/',
+            debug: true
+        };
+        const dl = new Downloader(options);
+
+        
+        const url = 'https://github.githubassets.com/images/icons/emoji/trollface.png';
+        const file_name = 'photo.png';
+        const path = './data/';
+        const opts = {};
+
+        dl.Download(url, file_name, opts, path);
+    })();
 ```
--- src/Downloader.js
+++ src/Downloader.js
@@ -1,61 +1,70 @@
 const fs = require('fs');
-const https = require('https');
 
 class Downloader {
+    /**
+     * Downloader class ⬇️
+     * @param {object} options
+     * @param {string} options.path - The path where the files will be downloaded
+     * @param {boolean} options.debug - If true, will print some debug messages 
+     */
     constructor(options) {
         this.base_path = options?.path ? (options.path.endsWith('/') ? options.path : options.path.concat('/')) : `${process.cwd()}/downloads/`;
         this.debug = options?.debug ? options.debug : true;
-    }
+    };
 
     /**
      * Downloads a file from the provided url
-     * @param {String} url
-     * @param {String} filename
-     * @param {Object} options
-     * @param {String} path
+     * @param {string} url The url of the file to download
+     * @param {string} [filename] OPTIONAL - The name of the file to be downloaded
+     * @param {object} [options] OPTIONAL - The options to be used for the download (http/https .request())
+     * @param {string} [path] OPTIONAL - The path where the file will be downloaded
      * @returns {Promise}
      */
 
-    Download (url, filename, options, path) {
-        
-        if (this.IsUrl(url) && url.startsWith('http')) {
+    Download(url, filename, options = {}, path) {
+        let get;
 
-            let dl_path = path ? (path.endsWith('/') ? path : path.concat('/')) : this.base_path;
-            let file_name = filename ? filename : url.split("/").pop();
-            const exist_path = this.CheckDir(dl_path);
-            
-            if (exist_path && file_name) {
+        if (!this.IsUrl(url)) {
+            if (this.debug) console.log(`[\x1b[31mERROR\x1b[0m]`, 'The url provided is not valid');
+            return new Error('The url provided is not valid');
+        };
 
-                return https.get(url, (res) => {
+        let dl_path = path ? (path.endsWith('/') ? path : path.concat('/')) : this.base_path;
+        let file_name = filename ? filename : url.split("/").pop();
+        const exist_path = this.#CheckDir(dl_path);
 
-                    const filePath = fs.createWriteStream(dl_path.concat(file_name));
-                    res.pipe(filePath);
-                    filePath.on('finish',() => {
-                        filePath.close();
-                        this.debug ? console.log(`[\x1b[32mSUCCESS\x1b[0m]`, `Downloaded "${file_name}" in "${dl_path}" !`) : "";
-                    })
-                })
-            }
-        } else {
-            return this.debug ? console.log(`[\x1b[31mERROR\x1b[0m]`, 'The url provided is not valid') : "";
-        }
+        if (exist_path && file_name) {
+
+            url.startsWith("https") ? get = require('https') : get = require('http');
+            return get.get(url, options, (res) => {
+
+                const filePath = fs.createWriteStream(dl_path.concat(file_name));
+                res.pipe(filePath);
+                filePath.on('finish', () => {
+                    filePath.close();
+                    if (this.debug) console.log(`[\x1b[32mSUCCESS\x1b[0m]`, `Downloaded "${file_name}" in "${dl_path}" !`);
+                });
+            });
+        };
     }
 
     /**
-    * @private Check if the dir exist, and if not tries to create it
-    * @param {String} path 
-    * @returns {Promise}
-    */
+     * @private Check if the dir exist, and if not tries to create it
+     * @param {string} path The path to the dir
+     * @returns {Promise}
+     */
 
-    async CheckDir (path) {
+    async #CheckDir(path) {
         if (fs.existsSync(path)) {
             return true;
         } else {
             try {
-                await fs.mkdirSync(path, { recursive: true});
+                await fs.mkdirSync(path, {
+                    recursive: true
+                });
                 return true;
             } catch (e) {
-                this.debug ? console.log(`[\x1b[31mERROR\x1b[0m]`, `Couldn't create "${path}" dir\n`, e.message) : "";
+                if (this.debug) console.log(`[\x1b[31mERROR\x1b[0m]`, `Couldn't create "${path}" dir\n`, e.message);
                 return false;
             }
         }
@@ -63,14 +72,14 @@ class Downloader {
 
     /**
      * @private Check if a string is an url
-     * @param {String} string The string to check
+     * @param {string} string The string to check
      * @returns {Boolean}
      */
-    
-     IsUrl (string) {
+
+    IsUrl(string) {
         const regex = new RegExp(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g);
-         return regex.test(string);
-     };
+        return regex.test(string);
+    };
 }
 
 module.exports = Downloader;
